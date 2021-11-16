8db8eb9240e659ce9c23b5b7cd978066c96c6bf5
jcaicedo <jose.caicedo@outlook.com>
2021-11-15 21:53:54 -0500

    consumo de la API

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,17 @@
-const Home = () =>{
+import getData from "../utils/getData";
+
+const Home = async () =>{
+  const characters = await getData();
   const view = `
     <article class="characters">
-      <div class="characters-item">
-        <a href="#/1/">
-          <img src="image" alt="name">
-          <h2>Name</h2>
-        </a>
-      </div>
+      ${characters.results.map(character => `
+        <div class="characters-item">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </div>
+      `).join('')}
     </article>
   `;
   return view;
