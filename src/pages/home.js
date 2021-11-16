75677cdf3315b62e9cfdd13956c8decec22c14b4
jcaicedo <jose.caicedo@outlook.com>
2021-11-15 19:24:56 -0500

    v1.0 estructura

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+const Home = () =>{
+  const view = `
+    <article class="characters">
+      <div class="characters-item">
+        <a href="#/1/">
+          <img src="image" alt="name">
+          <h2>Name</h2>
+        </a>
+      </div>
+    </article>
+  `;
+  return view;
+}
+
+export default Home;
---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+  const view = `
+    <header class="header_main">
+      <div class="header_logo">
+        <h1>
+          <a href="/">
+            100Tifi.co
+          </a>
+        </h1>
+      </div>
+      <div class="header_nav">
+        <a href="#/about/">
+          About
+        </a>
+      </div>
+    </header>
+  `;
+  return view
+};
+
+export default Header;
